30b897b1ab2b225c968be7a72b142b3b034f18d5
thomasstorm <thomas_storm@brockmann-consult.de>
2010-12-01 09:36:45 +0100

    added vgt_p spectral function dataset to script

--- src/main/sh/create_auxdata_files.sh
+++ src/main/sh/create_auxdata_files.sh
@@ -2,3 +2,4 @@ ncgen -k3 -o ./../../test/resources/syn/auxdata/syn_auxdata.nc ./../../test/reso
 ncgen -k3 -o ./../../test/resources/syn/auxdata/syn_config_params.nc ./../../test/resources/syn/auxdata/syn_config_params.cdl
 ncgen -k3 -o ./../../test/resources/syn/auxdata/vgt_s_auxdata.nc ./../../test/resources/syn/auxdata/vgt_s_auxdata.cdl
 ncgen -k3 -o ./../../test/resources/syn/auxdata/vgt_p_auxdata.nc ./../../test/resources/syn/auxdata/vgt_p_auxdata.cdl
+ncgen -k3 -o ./../../test/resources/syn/auxdata/vgt_p_spectral_response_function.nc ./../../test/resources/syn/auxdata/vgt_p_spectral_response_function.cdl
