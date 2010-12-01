85ea9919f4656f88baf8e49de5a5b9f42e44bf35
thomasstorm <thomas_storm@brockmann-consult.de>
2010-12-01 09:32:53 +0100

    using correct file format for ncgen

--- src/main/sh/create_auxdata_files.sh
+++ src/main/sh/create_auxdata_files.sh
@@ -1,4 +1,4 @@
-ncgen -k4 -o ./../../test/resources/syn/auxdata/syn_auxdata.nc ./../../test/resources/syn/auxdata/syn_auxdata.cdl
-ncgen -k4 -o ./../../test/resources/syn/auxdata/syn_config_params.nc ./../../test/resources/syn/auxdata/syn_config_params.cdl
-ncgen -k4 -o ./../../test/resources/syn/auxdata/vgt_s_auxdata.nc ./../../test/resources/syn/auxdata/vgt_s_auxdata.cdl
-ncgen -k4 -o ./../../test/resources/syn/auxdata/vgt_p_auxdata.nc ./../../test/resources/syn/auxdata/vgt_p_auxdata.cdl
+ncgen -k3 -o ./../../test/resources/syn/auxdata/syn_auxdata.nc ./../../test/resources/syn/auxdata/syn_auxdata.cdl
+ncgen -k3 -o ./../../test/resources/syn/auxdata/syn_config_params.nc ./../../test/resources/syn/auxdata/syn_config_params.cdl
+ncgen -k3 -o ./../../test/resources/syn/auxdata/vgt_s_auxdata.nc ./../../test/resources/syn/auxdata/vgt_s_auxdata.cdl
+ncgen -k3 -o ./../../test/resources/syn/auxdata/vgt_p_auxdata.nc ./../../test/resources/syn/auxdata/vgt_p_auxdata.cdl
