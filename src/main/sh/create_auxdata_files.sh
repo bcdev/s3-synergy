b9a6701a24c31211dca8d009f47fc5d32cc0cddd
thomasstorm <thomas_storm@brockmann-consult.de>
2010-12-01 11:03:02 +0100

    extended script

--- src/main/sh/create_auxdata_files.sh
+++ src/main/sh/create_auxdata_files.sh
@@ -3,3 +3,4 @@ ncgen -k3 -o ./../../test/resources/syn/auxdata/syn_config_params.nc ./../../tes
 ncgen -k3 -o ./../../test/resources/syn/auxdata/vgt_s_auxdata.nc ./../../test/resources/syn/auxdata/vgt_s_auxdata.cdl
 ncgen -k3 -o ./../../test/resources/syn/auxdata/vgt_p_auxdata.nc ./../../test/resources/syn/auxdata/vgt_p_auxdata.cdl
 ncgen -k3 -o ./../../test/resources/syn/auxdata/vgt_p_spectral_response_function.nc ./../../test/resources/syn/auxdata/vgt_p_spectral_response_function.cdl
+ncgen -k3 -o ./../../test/resources/syn/auxdata/vgt_s_config_params.nc ./../../test/resources/syn/auxdata/vgt_s_config_params.cdl
