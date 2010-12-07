9f24b631a4177f229dd05cf0b271651225202399
thomasstorm <thomas_storm@brockmann-consult.de>
2010-12-07 13:22:19 +0100

    corrected some auxdata definitions

--- src/main/sh/create_auxdata_files.sh
+++ src/main/sh/create_auxdata_files.sh
@@ -1,5 +1,5 @@
-ncgen -k3 -o ./../../test/resources/syn/auxdata/syn_auxdata.nc ./../../test/resources/syn/auxdata/syn_auxdata.cdl
-ncgen -k3 -o ./../../test/resources/syn/auxdata/syn_config_params.nc ./../../test/resources/syn/auxdata/syn_config_params.cdl
+ncgen -k3 -o ./../../test/resources/syn/auxdata/S3__SY_2_SYRTAX_20101201T000000_20120101T000000_20101207T120000__BC__D_NT_AUX_01.nc ./../../test/resources/syn/auxdata/syn_auxdata.cdl
+ncgen -k3 -o ./../../test/resources/syn/auxdata/S3__SY_2_SYCPAX_20101201T000000_20120101T000000_20101207T120000__BC__D_NT_AUX_01.nc ./../../test/resources/syn/auxdata/syn_config_params.cdl
 ncgen -k3 -o ./../../test/resources/syn/auxdata/vgt_s_auxdata.nc ./../../test/resources/syn/auxdata/vgt_s_auxdata.cdl
 ncgen -k3 -o ./../../test/resources/syn/auxdata/vgt_p_auxdata.nc ./../../test/resources/syn/auxdata/vgt_p_auxdata.cdl
 ncgen -k3 -o ./../../test/resources/syn/auxdata/vgt_p_spectral_response_function.nc ./../../test/resources/syn/auxdata/vgt_p_spectral_response_function.cdl
