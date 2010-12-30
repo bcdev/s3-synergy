0a27caca1a3fa42018801d3bee10a34934348c82
thomasstorm <thomas_storm@brockmann-consult.de>
2010-12-30 10:26:19 +0100

    processor now calls start and stop methods

--- src/main/cpp/util/Processor.cpp
+++ src/main/cpp/util/Processor.cpp
@@ -21,6 +21,9 @@ Processor::~Processor() {
 
 void Processor::process(Context& context) {
     vector<Module*> modules = context.getModules();
+    for (size_t i = 0; i < modules.size(); i++) {
+        modules[i]->start(context);
+    }
     while (!context.isCompleted()) {
         for (size_t i = 0; i < modules.size(); i++) {
             modules[i]->process(context);
