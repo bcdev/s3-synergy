ec384b7efc4371dce33633f7cceaee136f464752
Ralf Quast <ralf.quast@brockmann-consult.de>
2011-01-04 18:28:32 +0100

    Update.

--- S3L2PP-util/src/main/java/org/esa/S3L2PP/TestDataGenerator.java
+++ S3L2PP-util/src/main/java/org/esa/S3L2PP/TestDataGenerator.java
@@ -20,6 +20,7 @@ public class TestDataGenerator {
         try {
             generateDummyOlciRadianceDatasets();
             generateDummySlstrRadianceDatasets();
+            generateDummyGeolocationDataset();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -32,6 +33,46 @@ public class TestDataGenerator {
     private static void generateDummySlstrRadianceDatasets() throws Exception {
         generateDatasets(1, 6, "SLST_NAD_RADIANCE_S", "testdata/cdl/dummy", "testdata/nc/dummy");
         generateDatasets(1, 6, "SLST_ALT_RADIANCE_S", "testdata/cdl/dummy", "testdata/nc/dummy");
+    }
+
+    private static void generateDummyGeolocationDataset() throws Exception {
+        generateDataset("GEOLOCATION_REF", "testdata/cdl/dummy", "testdata/nc/dummy");
+    }
+
+    private static void generateDataset(String template, String cdlPath, String ncPath) throws Exception {
+        final File cdlFile = new File(cdlPath, template + ".cdl");
+        BufferedReader reader = null;
+        BufferedWriter writer = null;
+        try {
+            final InputStream is = TestDataGenerator.class.getResourceAsStream(template + ".cdl");
+            reader = new BufferedReader(new InputStreamReader(is, "US-ASCII"));
+            writer = new BufferedWriter(new FileWriter(cdlFile));
+            String line = reader.readLine();
+            while (line != null) {
+                writer.write(line + "\n");
+                line = reader.readLine();
+            }
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                // ignore
+            }
+        }
+        final File ncFile = new File(ncPath, cdlFile.getName().replace(".cdl", ".nc"));
+        final String ncgenPath = System.getProperty("ncgenPath", NCGEN_PATH_DEFAULT);
+        final String command = ncgenPath + " -k 3 -o " + ncFile.getPath() + " " + cdlFile.getPath();
+        final Process process = Runtime.getRuntime().exec(command);
+        if (process.waitFor() != 0) {
+            throw new Exception(
+                    MessageFormat.format("process <code>{0}</code> terminated with exit value {1}",
+                                         command, process.exitValue()));
+        }
     }
 
     private static void generateDatasets(int first, int last, String template, String cdlPath,
