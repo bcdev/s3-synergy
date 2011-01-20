b3c4bd4d7d4fb49b9b241f5dabbc7a1bace7e752
thomasstorm <thomas_storm@brockmann-consult.de>
2011-01-20 15:38:07 +0100

    extended generator to generate L1C-dictionary files

--- S3L2PP-util/src/main/java/org/esa/S3L2PP/DictionaryGenerator.java
+++ S3L2PP-util/src/main/java/org/esa/S3L2PP/DictionaryGenerator.java
@@ -30,29 +30,115 @@ import java.util.Properties;
 
 public class DictionaryGenerator {
 
-    private static final String TARGET_DIR_DEFAULT = "C:\\dev\\projects\\S3L2PP\\src\\main\\resources\\dictionary";
-    private static File targetDir;
+    private static final int L1C = 1;
+    private static final int L2 = 2;
 
     public static void main(String[] args) throws Exception {
-        targetDir = new File(System.getProperty("targetDir", TARGET_DIR_DEFAULT));
+        generateDatasets(L1C, "GEN_INFO_OLC_", 1, 18);
+        generateDatasets(L1C, "GEN_INFO_SLST_ALT_S", 25, 30, 1);
+        generateDatasets(L1C, "GEN_INFO_SLST_NAD_S", 19, 24, 1);
+        generateDataset(L1C, "GEOLOCATION_REF");
+        int[] excludes = new int[]{14, 15, 17, 20};
+        generateDatasets(L1C, "MISREGIST_OLC_Oref_O", 1, 21, 1, excludes);
+        generateDatasets(L1C, "OLC_RADIANCE_O", 1, 21, 1, excludes);
+        generateDataset(L1C, "PIX_ANNOT_OLC");
+        generateDataset(L1C, "PIX_ANNOT_SLST_ALT_A");
+        generateDataset(L1C, "PIX_ANNOT_SLST_NAD_A");
+        generateSLSTRadianceDatasets("SLST_ALT_RADIANCE_S", 24);
+        generateSLSTRadianceDatasets("SLST_NAD_RADIANCE_S", 18 );
+        generateDataset(L1C, "SUBS_ANNOT_GEOM_OLC");
+        generateDataset(L1C, "SUBS_ANNOT_METEO_OLC");
+        generateDataset(L1C, "SUBS_ANNOT_SLST_ALT");
+        generateDataset(L1C, "SUBS_ANNOT_SLST_NAD");
+        generateDataset(L1C, "TIME_STAMP_OLC");
+
         generateSDRDataset(1, 30);
-        generateDataset("L2_SYN_aerosol_angstrom_exponent", "a550");
-        generateDataset("L2_SYN_aerosol_model_index_number", "amin");
-        generateDataset("L2_SYN_aerosol_optical_thickness", "t550");
-        generateDataset("L2_SYN_geo-location", "geolocation");
-        generateDataset("L2_SYN_geophysical_atmospheric_data", "tiepoints_meteo");
-        generateDataset("L2_SYN_OLCI_sub-sampled_annotations", "tiepoints_olci");
-        generateDataset("L2_SYN_SLSTR_nadir_sub-sampled_annotations", "tiepoints_slstr_n");
-        generateDataset("L2_SYN_SLSTR_oblique_sub-sampled_annotations", "tiepoints_slstr_o");
-        generateDataset("L2_SYN_status_flags", "flags");
-        generateDataset("L2_SYN_time_stamps", "time");
+        generateDataset(L2, "L2_SYN_aerosol_angstrom_exponent", "a550");
+        generateDataset(L2, "L2_SYN_aerosol_model_index_number", "amin");
+        generateDataset(L2, "L2_SYN_aerosol_optical_thickness", "t550");
+        generateDataset(L2, "L2_SYN_geo-location", "geolocation");
+        generateDataset(L2, "L2_SYN_geophysical_atmospheric_data", "tiepoints_meteo");
+        generateDataset(L2, "L2_SYN_OLCI_sub-sampled_annotations", "tiepoints_olci");
+        generateDataset(L2, "L2_SYN_SLSTR_nadir_sub-sampled_annotations", "tiepoints_slstr_n");
+        generateDataset(L2, "L2_SYN_SLSTR_oblique_sub-sampled_annotations", "tiepoints_slstr_o");
+        generateDataset(L2, "L2_SYN_status_flags", "flags");
+        generateDataset(L2, "L2_SYN_time_stamps", "time");
     }
 
-    private static void generateDataset(String templateName, String datasetName) throws Exception {
+    private static void generateSLSTRadianceDatasets(String templateName, int offset) throws Exception {
+        String targetDir = "C:\\dev\\projects\\S3L2PP\\src\\main\\resources\\dictionary\\L1C";
         final Properties properties = new Properties();
+        properties.setProperty("target_dir", targetDir);
+        properties.setProperty("Template_File_Basename", templateName);
+        for (int i = 1; i <= 6; i++) {
+            properties.setProperty("i", String.format("%d", i));
+            properties.setProperty("j", String.format("%d", i + offset));
+            properties.setProperty("dataset_name", templateName + i);
+            generateDataset(properties);
+        }
+    }
+
+    private static void generateDatasets(int level, String templateName, int minChannel, int maxChannel, int start,
+                                         int[] excludes) throws Exception {
+        String targetDir = "";
+        if (level == L1C) {
+            targetDir = "C:\\dev\\projects\\S3L2PP\\src\\main\\resources\\dictionary\\L1C";
+        } else if (level == L2) {
+            targetDir = "C:\\dev\\projects\\S3L2PP\\src\\main\\resources\\dictionary\\L2";
+        }
+        final Properties properties = new Properties();
+        properties.setProperty("target_dir", targetDir);
+        properties.setProperty("Template_File_Basename", templateName);
+        int counter = 0;
+        for (int i = minChannel; i <= maxChannel; i++) {
+            if (isExcluded(excludes, i)) {
+                start++;
+                i++;
+                counter++;
+            }
+            properties.setProperty("i", String.format("%d", i - counter));
+            properties.setProperty("dataset_name", templateName + start);
+            generateDataset(properties);
+            start++;
+        }
+    }
+
+    private static boolean isExcluded(int[] excludes, int i) {
+        for (int exclude : excludes) {
+            if (i == exclude) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void generateDatasets(int level, String templateName, int minChannel, int maxChannel,
+                                         int start) throws Exception {
+        generateDatasets(level, templateName, minChannel, maxChannel, start, new int[0]);
+
+    }
+
+    private static void generateDataset(int level, String templateName) throws Exception {
+        generateDataset(level, templateName, templateName);
+    }
+
+    private static void generateDataset(int level, String templateName, String datasetName) throws Exception {
+        final Properties properties = new Properties();
+        String targetDir = "";
+        if (level == L1C) {
+            targetDir = "C:\\dev\\projects\\S3L2PP\\src\\main\\resources\\dictionary\\L1C";
+        } else if (level == L2) {
+            targetDir = "C:\\dev\\projects\\S3L2PP\\src\\main\\resources\\dictionary\\L2";
+        }
+        properties.setProperty("target_dir", targetDir);
         properties.setProperty("Template_File_Basename", templateName);
         properties.setProperty("dataset_name", datasetName);
         generateDataset(properties);
+    }
+
+    private static void generateDatasets(int level, String templateName, int minChannel, int maxChannel) throws
+                                                                                                         Exception {
+        generateDatasets(level, templateName, minChannel, maxChannel, minChannel);
     }
 
     private static void generateSDRDataset(int minChannel, int maxChannel) throws Exception {
@@ -60,6 +146,7 @@ public class DictionaryGenerator {
         final CsvReader csvReader = new CsvReader(new InputStreamReader(csv), new char[]{','});
         final List<String[]> wavelengths = csvReader.readStringRecords();
         final Properties properties = new Properties();
+        properties.setProperty("target_dir", "C:\\dev\\projects\\S3L2PP\\src\\main\\resources\\dictionary\\L2");
         properties.setProperty("Template_File_Basename", "L2_SYN_Surface_Directional_Reflectance");
         properties.setProperty("SDR", "SDR_${i}");
         properties.setProperty("SDR_er", "SDR_${i}_er");
@@ -85,6 +172,7 @@ public class DictionaryGenerator {
 
     private static void generateDataset(Properties properties) throws Exception {
         final TemplateResolver resolver = new TemplateResolver(properties);
+        String targetDir = properties.getProperty("target_dir");
         final File targetFile = new File(targetDir, resolver.resolveProperty("dataset_name") + ".xml");
         BufferedReader reader = null;
         BufferedWriter writer = null;
