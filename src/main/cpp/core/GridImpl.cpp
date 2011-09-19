9c7178ccdd0f05a32c203389f187685c77432cf2
thomasstorm <thomas.storm@brockmann-consult.de>
2011-09-19 18:04:20 +0200

    more verbose exception

--- src/main/cpp/core/GridImpl.cpp
+++ src/main/cpp/core/GridImpl.cpp
@@ -73,13 +73,13 @@ void GridImpl::setFirstL(size_t l) {
 
 size_t GridImpl::getIndex(size_t k, size_t l, size_t m) const throw (out_of_range) {
     if (k < firstK || k > firstK + sizeK - 1) {
-        BOOST_THROW_EXCEPTION(out_of_range("index k is out of range."));
+        BOOST_THROW_EXCEPTION(out_of_range("index k is out of range: k=" + lexical_cast<string>(k) + ", firstK=" + lexical_cast<string>(firstK) + ", sizeK=" + lexical_cast<string>(sizeK)));
     }
     if (l < firstL || l > firstL + sizeL - 1) {
-        BOOST_THROW_EXCEPTION(out_of_range("index l is out of range."));
+        BOOST_THROW_EXCEPTION(out_of_range("index l is out of range: l=" + lexical_cast<string>(l) + ", firstL=" + lexical_cast<string>(firstL) + ", sizeL=" + lexical_cast<string>(sizeL)));
     }
     if (m < firstM || m > firstM + sizeM - 1) {
-        BOOST_THROW_EXCEPTION(out_of_range("index m is out of range."));
+        BOOST_THROW_EXCEPTION(out_of_range("index m is out of range: m=" + lexical_cast<string>(m) + ", firstM=" + lexical_cast<string>(firstM) + ", sizeM=" + lexical_cast<string>(sizeM)));
     }
     return (k - firstK) * strideK + (l - firstL) * strideL + (m - firstM) * strideM;
 }
