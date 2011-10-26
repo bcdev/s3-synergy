389603d347d3be9a83c1674fdfa7322d954acbf3
Ralf Quast <ralf.quast@brockmann-consult.de>
2011-10-26 14:19:54 +0200

    Update

--- src/main/cpp/core/Constants.h
+++ src/main/cpp/core/Constants.h
@@ -23,7 +23,18 @@
 
 #include <string>
 
+#include <boost/cstdint.hpp>
+
 using std::string;
+
+using boost::int8_t;
+using boost::int16_t;
+using boost::int32_t;
+using boost::int64_t;
+using boost::uint8_t;
+using boost::uint16_t;
+using boost::uint32_t;
+using boost::uint64_t;
 
 namespace Constants {
 
