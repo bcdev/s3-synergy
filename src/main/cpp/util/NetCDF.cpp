9ed98cb0df7fc21efc995f15caaa7b5add967a22
Ralf Quast <ralf.quast@brockmann-consult.de>
2011-11-17 11:11:05 +0100

    update

--- src/main/cpp/util/NetCDF.cpp
+++ src/main/cpp/util/NetCDF.cpp
@@ -236,10 +236,11 @@ int NetCDF::defineDimension(int fileId, const string& dimName, size_t size) {
 
 int NetCDF::defineVariable(int fileId, const string& varName, int type, const valarray<int>& dimIds, bool compress) {
 	int varId;
-	const int status = nc_def_var(fileId, varName.c_str(), type, dimIds.size(), &dimIds[0], &varId);
+	int status = nc_def_var(fileId, varName.c_str(), type, dimIds.size(), &dimIds[0], &varId);
 	checkStatus(status, "defining variable");
 	if (compress) {
-		nc_def_var_deflate(fileId, varId, 1, 1, 7);
+		status = nc_def_var_deflate(fileId, varId, 1, 1, 7);
+		checkStatus(status, "defining compression parameters");
 	}
 	return varId;
 }
