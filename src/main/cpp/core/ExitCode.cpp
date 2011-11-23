4adad78a6e9e5e0553010a6711b5cbdc07d63491
thomasstorm <thomas.storm@brockmann-consult.de>
2011-11-23 16:50:36 +0100

    added ExitCode.cpp apparently needed for some compiler versions

---
+++ src/main/cpp/core/ExitCode.cpp
@@ -0,0 +1,25 @@
+/* 
+ * Copyright (C) 2011 by Brockmann Consult (info@brockmann-consult.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation. This program is distributed in the hope it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ *
+ * File:   ExitCode.cpp
+ * Author: thomasstorm
+ * 
+ * Created on November 23, 2011
+ */
+
+#include "ExitCode.h"
+
+const int ExitCode::OK = 0;
+const int ExitCode::INCOMPLETE = 1;
+const int ExitCode::FAILURE = 128;
