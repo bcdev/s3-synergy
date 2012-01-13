90b74b743d0db5c3cb80676c55f258b8fc257b82
thomasstorm <thomas.storm@brockmann-consult.de>
2012-01-13 11:55:37 +0100

    introduced VgtSWriter

---
+++ src/main/cpp/writer/VgtSWriter.h
@@ -0,0 +1,27 @@
+/*
+ * File:   VgtSWriter.h
+ * Author: thomass
+ *
+* Created on January 13, 2012, 11:34
+ */
+
+#ifndef VGTSWRITER_H
+#define	VGTSWRITER_H
+
+#include "../writer/AbstractWriter.h"
+
+class VgtSWriter: public AbstractWriter {
+public:
+    VgtSWriter();
+    virtual ~VgtSWriter();
+
+protected:
+    const string getProductDescriptorIdentifier() const;
+    const string getSafeManifestName() const;
+
+private:
+	friend class VgtSWriterTest;
+};
+
+#endif	/* VGTSWRITER_H */
+
