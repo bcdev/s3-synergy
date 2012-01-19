f0f5096b0e6d156f17abb8534fb219ec71a5b8ca
Ralf Quast <ralf.quast@brockmann-consult.de>
2012-01-19 09:45:14 +0100

    Update

--- src/test/cpp/util/PixelFinderTest.cpp
+++ src/test/cpp/util/PixelFinderTest.cpp
@@ -5,10 +5,42 @@
  *      Author: ralf
  */
 
+#include "../../../main/cpp/core/GridImpl.h"
+#include "../../../main/cpp/util/PixelFinder.h"
+
 #include "PixelFinderTest.h"
 
 CPPUNIT_TEST_SUITE_REGISTRATION(PixelFinderTest);
 
+class GeoLocationProvider : public GeoLocation {
+public:
+
+	GeoLocationProvider() : grid(4, 6000, 740, 0, 5999) {
+	}
+
+	~GeoLocationProvider() {
+
+	}
+
+	const Grid& getGrid() const {
+		return grid;
+	}
+
+	double getLat(size_t index) const {
+		long k = index / grid.getStrideK();
+		long l = (index - k * grid.getStrideK()) / grid.getStrideL();
+		return 90.0 - l * (180.0/grid.getMaxL());
+	}
+
+	double getLon(size_t index) const {
+		long k = index / grid.getStrideK();
+		long m = index % grid.getSizeM();
+		return m * (22.2/grid.getMaxM()) - 44.4 + k * 22.2;
+	}
+
+private:
+	GridImpl grid;
+};
 
 PixelFinderTest::PixelFinderTest() {
 }
@@ -30,6 +62,13 @@ void PixelFinderTest::testFindPixel() {
 	long k = 0;
 	long l = 0;
 	long m = 0;
+
+	pixelFinder.findSourcePixel(90.0, -44.4, k, l, m);
+
+	CPPUNIT_ASSERT(k == 0);
+	CPPUNIT_ASSERT(l == 0);
+	CPPUNIT_ASSERT(m == 0);
+
 	pixelFinder.findSourcePixel(90.0, -44.4, k, l, m);
 
 	CPPUNIT_ASSERT(k == 0);
--- src/test/cpp/util/PixelFinderTest.h
+++ src/test/cpp/util/PixelFinderTest.h
@@ -10,39 +10,6 @@
 
 #include <cppunit/extensions/HelperMacros.h>
 
-#include "../../../main/cpp/core/GridImpl.h"
-#include "../../../main/cpp/util/PixelFinder.h"
-
-class GeoLocationProvider : public GeoLocation {
-public:
-
-	GeoLocationProvider() : grid(4, 6000, 740, 0, 5999) {
-	}
-
-	~GeoLocationProvider() {
-
-	}
-
-	const Grid& getGrid() const {
-		return grid;
-	}
-
-	double getLat(size_t index) const {
-		long k = index / grid.getStrideK();
-		long l = (index - k * grid.getStrideK()) / grid.getStrideL();
-		return 90.0 - l * (180.0/grid.getMaxL());
-	}
-
-	double getLon(size_t index) const {
-		long k = index / grid.getStrideK();
-		long m = index % grid.getSizeM();
-		return m * (22.2/grid.getMaxM()) - 44.4 + k * 22.2;
-	}
-
-private:
-	GridImpl grid;
-};
-
 class PixelFinderTest: public CPPUNIT_NS::TestFixture {
 CPPUNIT_TEST_SUITE(PixelFinderTest);
 		CPPUNIT_TEST(testFindPixel);
