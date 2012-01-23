52f7bcb6f92dbf8f445c0775eb67a1c5a72782ee
Ralf Quast <ralf.quast@brockmann-consult.de>
2012-01-23 13:24:43 +0100

    consolidated PixelFinder

--- src/main/cpp/util/PixelFinder.cpp
+++ src/main/cpp/util/PixelFinder.cpp
@@ -50,83 +50,69 @@ PixelFinder::~PixelFinder() {
 	delete tpi;
 }
 
-bool PixelFinder::findSourcePixel(double targetLat, double targetLon, long& sourceK, long& sourceL, long& sourceM) const {
+bool PixelFinder::findSourcePixel(double targetLat, double targetLon, long& k, long& l, long& m) const {
+	using std::acos;
+	using std::max;
+	using std::min;
+
 	const Grid& grid = geoLocation.getGrid();
 
 	valarray<double> w(1);
 	valarray<size_t> i(1);
 	tpi->prepare(targetLon, targetLat, w, i);
 
-	const long strideK = grid.getStrideK();
-	const long strideL = grid.getStrideL();
-
 	const size_t index = tpi->interpolate(tpIndices, w, i);
-	sourceK = index / strideK;
-	sourceL = (index - sourceK * strideK) / strideL;
-	sourceM = index % grid.getSizeM();
-
-	return findSourcePixel(targetLat, targetLon, index, sourceK, sourceL, sourceM);
-}
-
-bool PixelFinder::findSourcePixel(double targetLat, double targetLon, size_t index, long& sourceK, long& sourceL, long& sourceM) const {
-	using std::acos;
-	using std::max;
-	using std::min;
-
-	const Grid& grid = geoLocation.getGrid();
-	const long sizeK = grid.getSizeK();
-	const long sizeM = grid.getSizeM();
+	k = getK(index);
+	l = getL(index);
+	m = getM(index);
 
 	double delta = -1.0;
-
-	isNearestPixel(targetLat, targetLon, sourceK, sourceL, sourceM, sourceK, sourceL, sourceM, delta);
+	updateNearestPixel(targetLat, targetLon, k, l, m, k, l, m, delta);
 
 	for (long b = 32; b > 0; b >>= 1) {
-		const long sourceN = sourceM + sourceK * sizeM;
+		const long n = getN(k, m);
 
-		const long minL = max(0L, sourceL - b);
-		const long maxL = min(grid.getMaxL(), sourceL + b);
-		const long minN = max(0L, sourceN - b);
-		const long maxN = min(sizeK * sizeM - 1, sourceN + b);
-		const long minK = minN / sizeM;
-		const long maxK = maxN / sizeM;
-		const long minM = minN % sizeM;
-		const long maxM = maxN % sizeM;
+		const long minL = max(grid.getMinL(), l - b);
+		const long maxL = min(grid.getMaxL(), l + b);
+		const long minN = max(getN(grid.getMinK(), grid.getMinM()), n - b);
+		const long maxN = min(getN(grid.getMaxK(), grid.getMaxM()), n + b);
 
-		if (true) {
-			const long centerK = sourceK;
-			const long centerL = sourceL;
-			const long centerM = sourceM;
+		const long minK = getK(minN);
+		const long maxK = getK(maxN);
+		const long minM = getM(minN);
+		const long maxM = getM(maxN);
 
-			isNearestPixel(targetLat, targetLon, minK, centerL, minM, sourceK, sourceL, sourceM, delta);
-			isNearestPixel(targetLat, targetLon, maxK, centerL, maxM, sourceK, sourceL, sourceM, delta);
-			isNearestPixel(targetLat, targetLon, centerK, maxL, centerM, sourceK, sourceL, sourceM, delta);
-			isNearestPixel(targetLat, targetLon, centerK, minL, centerM, sourceK, sourceL, sourceM, delta);
+		if (true) { // consider points in the N, S, E, and W
+			const long midK = k;
+			const long midL = l;
+			const long midM = m;
+
+			updateNearestPixel(targetLat, targetLon, minK, midL, minM, k, l, m, delta);
+			updateNearestPixel(targetLat, targetLon, maxK, midL, maxM, k, l, m, delta);
+			updateNearestPixel(targetLat, targetLon, midK, maxL, midM, k, l, m, delta);
+			updateNearestPixel(targetLat, targetLon, midK, minL, midM, k, l, m, delta);
 		}
-
-		isNearestPixel(targetLat, targetLon, minK, minL, minM, sourceK, sourceL, sourceM, delta);
-		isNearestPixel(targetLat, targetLon, minK, maxL, minM, sourceK, sourceL, sourceM, delta);
-		isNearestPixel(targetLat, targetLon, maxK, maxL, maxM, sourceK, sourceL, sourceM, delta);
-		isNearestPixel(targetLat, targetLon, maxK, minL, maxM, sourceK, sourceL, sourceM, delta);
+		if (true) { // consider points in the NW, SW, SE, and NE
+			updateNearestPixel(targetLat, targetLon, minK, minL, minM, k, l, m, delta);
+			updateNearestPixel(targetLat, targetLon, minK, maxL, minM, k, l, m, delta);
+			updateNearestPixel(targetLat, targetLon, maxK, maxL, maxM, k, l, m, delta);
+			updateNearestPixel(targetLat, targetLon, maxK, minL, maxM, k, l, m, delta);
+		}
 	}
 
-    return acos(delta) * DEG < 1.41 * pixelSize;
+    return acos(delta) * DEG < 1.4 * pixelSize;
 }
 
-bool PixelFinder::isNearestPixel(double targetLat, double targetLon, long k, long l, long m, long& resultK, long& resultL, long& resultM, double& maxDelta) const {
-	using std::abs;
-
+void PixelFinder::updateNearestPixel(double targetLat, double targetLon, long k, long l, long m, long& resultK, long& resultL, long& resultM, double& maxDelta) const {
 	const size_t index = geoLocation.getGrid().getIndex(k, l, m);
 	const double sourceLat = geoLocation.getLat(index);
 	const double sourceLon = geoLocation.getLon(index);
-
 	const double delta = TiePointInterpolator<double>::cosineDistance(targetLon, targetLat, sourceLon, sourceLat);
+
 	if (delta > maxDelta) {
 		resultK = k;
 		resultL = l;
 		resultM = m;
 		maxDelta = delta;
-		return true;
 	}
-	return false;
 }
