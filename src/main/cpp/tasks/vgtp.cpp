f0986e243f46add68090e6d5774228bd0e4d26fe
thomasstorm <thomas.storm@brockmann-consult.de>
2012-01-24 15:22:10 +0100

    added vgt-p main executable

---
+++ src/main/cpp/tasks/vgtp.cpp
@@ -0,0 +1,38 @@
+/*
+ * vgtp.cpp
+ *
+ *  Created on: Jan 24, 2012
+ *      Author: thomasstorm
+ */
+
+#include "../modules/Col.h"
+#include "../modules/Pcl.h"
+#include "../modules/Vbm.h"
+#include "../modules/Vfl.h"
+#include "../modules/Vpr.h"
+#include "../reader/SynL1Reader.h"
+#include "../writer/VgtPWriter.h"
+
+#include "../util/BasicTask.h"
+
+int main(int argc, char* argv[]) {
+	BasicTask task("VGTP");
+
+	shared_ptr<Module> reader = shared_ptr<Module>(new SynL1Reader());
+	shared_ptr<Module> col = shared_ptr<Module>(new Col());
+	shared_ptr<Module> pcl = shared_ptr<Module>(new Pcl());
+	shared_ptr<Module> vbm = shared_ptr<Module>(new Vbm());
+	shared_ptr<Module> vfl = shared_ptr<Module>(new Vfl());
+	shared_ptr<Module> vpr = shared_ptr<Module>(new Vpr());
+	shared_ptr<Module> writer = shared_ptr<Module>(new VgtPWriter());
+
+	task.getContext().addModule(reader);
+	task.getContext().addModule(col);
+	task.getContext().addModule(pcl);
+	task.getContext().addModule(vbm);
+	task.getContext().addModule(vfl);
+	task.getContext().addModule(vpr);
+	task.getContext().addModule(writer);
+
+	return task.execute(argc, argv);
+}
