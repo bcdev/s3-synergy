83b21e7149bc9b4d8b26b061678ee785309dec84
thomasstorm <thomas.storm@brockmann-consult.de>
2012-01-26 15:37:22 +0100

    fix

--- src/main/cpp/modules/Vbm.h
+++ src/main/cpp/modules/Vbm.h
@@ -57,7 +57,7 @@ private:
 	void prepareTiePointData(Context& context);
     void addTargetVariables(Context& context);
 
-	void initPixel(Pixel& p, size_t index, valarray<double>& tpiWeights, valarray<size_t>& tpiIndexes);
+	void initPixel(Pixel& p, size_t index, size_t geoIndex, valarray<double>& tpiWeights, valarray<size_t>& tpiIndexes);
 	void performDownscaling(const Pixel& p, valarray<double>& synSurfaceReflectances, valarray<double>& coordinates, valarray<double>& f, valarray<double>& w);
 	void performHyperspectralInterpolation(const valarray<double>& synWavelengths, const valarray<double>& synSurfaceReflectances, valarray<double>& hypSurfaceReflectances);
 	void performHyperspectralUpscaling(const valarray<double>& hypSurfaceReflectances, const Pixel& p, valarray<double>& hypToaReflectances, valarray<double>& coordinates, valarray<double>& f, valarray<double>& w);
