601e169f708950a9ed70b520a18cc75712a4f8f9
thomasstorm <thomas.storm@brockmann-consult.de>
2012-02-07 17:52:39 +0100

    fix

--- src/test/cpp/modules/VcoTest.h
+++ src/test/cpp/modules/VcoTest.h
@@ -13,7 +13,6 @@
 class VcoTest : public CPPUNIT_NS::TestFixture {
     CPPUNIT_TEST_SUITE(VcoTest);
     CPPUNIT_TEST(testVco);
-    CPPUNIT_TEST(testTimeConversion);
     CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -24,7 +23,6 @@ public:
 
 private:
     void testVco();
-    void testTimeConversion();
 };
 
 #endif	/* VCOTEST_H */
