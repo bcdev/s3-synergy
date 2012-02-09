b5163d65041bba28f559a104d7c15efb10795605
thomasstorm <thomas.storm@brockmann-consult.de>
2012-02-09 10:04:49 +0100

    format

--- src/main/cpp/reader/SynL1Reader.cpp
+++ src/main/cpp/reader/SynL1Reader.cpp
@@ -59,77 +59,75 @@ void SynL1Reader::start(Context& context) {
 	}
 	context.getLogging().info("Source product path is '" + sourceDirPath.string() + "'", getId());
 
-	foreach(SegmentDescriptor* segmentDescriptor, segmentDescriptors)
-			{
-				const vector<VariableDescriptor*> variableDescriptors = segmentDescriptor->getVariableDescriptors();
-				const string& segmentName = segmentDescriptor->getName();
+	foreach(SegmentDescriptor* segmentDescriptor, segmentDescriptors) {
+	    const vector<VariableDescriptor*> variableDescriptors = segmentDescriptor->getVariableDescriptors();
+	    const string& segmentName = segmentDescriptor->getName();
 
-				foreach(VariableDescriptor* variableDescriptor, variableDescriptors)
-						{
-							const string varName = variableDescriptor->getName();
-							const string ncVarName = variableDescriptor->getNcVarName();
-							const string ncFileBasename = variableDescriptor->getNcFileBasename();
-							const int fileId = getNcFile(ncFileBasename);
-							const int varId = NetCDF::getVariableId(fileId, ncVarName.c_str());
-							const int dimCount = NetCDF::getDimensionCount(fileId, varId);
-							const valarray<int> dimIds = NetCDF::getDimensionIds(fileId, varId);
+	    foreach(VariableDescriptor* variableDescriptor, variableDescriptors) {
+	        const string varName = variableDescriptor->getName();
+	        const string ncVarName = variableDescriptor->getNcVarName();
+	        const string ncFileBasename = variableDescriptor->getNcFileBasename();
+	        const int fileId = getNcFile(ncFileBasename);
+	        const int varId = NetCDF::getVariableId(fileId, ncVarName.c_str());
+	        const int dimCount = NetCDF::getDimensionCount(fileId, varId);
+	        const valarray<int> dimIds = NetCDF::getDimensionIds(fileId, varId);
 
-							long camCount;
-							long rowCount;
-							long colCount;
+	        long camCount;
+	        long rowCount;
+	        long colCount;
 
-							// Set grid parameters and write them to dictionary
-							switch (dimCount) {
-							case 3:
-								camCount = NetCDF::getDimensionLength(fileId, dimIds[0]);
-								rowCount = NetCDF::getDimensionLength(fileId, dimIds[1]);
-								colCount = NetCDF::getDimensionLength(fileId, dimIds[2]);
-								variableDescriptor->addDimension(NetCDF::getDimensionName(fileId, dimIds[0])).setSize(camCount);
-								variableDescriptor->addDimension(NetCDF::getDimensionName(fileId, dimIds[1])).setSize(rowCount);
-								variableDescriptor->addDimension(NetCDF::getDimensionName(fileId, dimIds[2])).setSize(colCount);
-								break;
-							case 2:
-								camCount = 1;
-								rowCount = NetCDF::getDimensionLength(fileId, dimIds[0]);
-								colCount = NetCDF::getDimensionLength(fileId, dimIds[1]);
-								variableDescriptor->addDimension(NetCDF::getDimensionName(fileId, dimIds[0])).setSize(rowCount);
-								variableDescriptor->addDimension(NetCDF::getDimensionName(fileId, dimIds[1])).setSize(colCount);
-								break;
-							case 1:
-								camCount = 1;
-								rowCount = 1;
-								colCount = NetCDF::getDimensionLength(fileId, dimIds[0]);
-								variableDescriptor->addDimension(NetCDF::getDimensionName(fileId, dimIds[0])).setSize(colCount);
-								break;
-							default:
-								BOOST_THROW_EXCEPTION( runtime_error("Invalid number of dimensions for variable '" + ncVarName + "'."));
-								break;
-							}
-							// Create a new segment, if necessary
-							if (!context.hasSegment(segmentName)) {
-								const long sizeL = getSegmentSize(segmentName, rowCount);
-								context.getLogging().info("Adding segment '" + segmentName + "' to context", getId());
-								context.addSwathSegment(segmentName, sizeL, colCount, camCount, 0, rowCount - 1);
-								addedSegments.push_back(segmentName);
-							}
+	        // Set grid parameters and write them to dictionary
+	        switch (dimCount) {
+	        case 3:
+	            camCount = NetCDF::getDimensionLength(fileId, dimIds[0]);
+	            rowCount = NetCDF::getDimensionLength(fileId, dimIds[1]);
+	            colCount = NetCDF::getDimensionLength(fileId, dimIds[2]);
+	            variableDescriptor->addDimension(NetCDF::getDimensionName(fileId, dimIds[0])).setSize(camCount);
+	            variableDescriptor->addDimension(NetCDF::getDimensionName(fileId, dimIds[1])).setSize(rowCount);
+	            variableDescriptor->addDimension(NetCDF::getDimensionName(fileId, dimIds[2])).setSize(colCount);
+	            break;
+	        case 2:
+	            camCount = 1;
+	            rowCount = NetCDF::getDimensionLength(fileId, dimIds[0]);
+	            colCount = NetCDF::getDimensionLength(fileId, dimIds[1]);
+	            variableDescriptor->addDimension(NetCDF::getDimensionName(fileId, dimIds[0])).setSize(rowCount);
+	            variableDescriptor->addDimension(NetCDF::getDimensionName(fileId, dimIds[1])).setSize(colCount);
+	            break;
+	        case 1:
+	            camCount = 1;
+	            rowCount = 1;
+	            colCount = NetCDF::getDimensionLength(fileId, dimIds[0]);
+	            variableDescriptor->addDimension(NetCDF::getDimensionName(fileId, dimIds[0])).setSize(colCount);
+	            break;
+	        default:
+	            BOOST_THROW_EXCEPTION( runtime_error("Invalid number of dimensions for variable '" + ncVarName + "'."));
+	            break;
+	        }
+	        // Create a new segment, if necessary
+	        if (!context.hasSegment(segmentName)) {
+	            const long sizeL = getSegmentSize(segmentName, rowCount);
+	            context.getLogging().info("Adding segment '" + segmentName + "' to context", getId());
+	            context.addSwathSegment(segmentName, sizeL, colCount, camCount, 0, rowCount - 1);
+	            addedSegments.push_back(segmentName);
+	        }
 
-							// Copy variable attributes to dictionary
-							const int type = NetCDF::getVariableType(fileId, varId);
-							variableDescriptor->setType(type);
-							const size_t attrCount = NetCDF::getAttributeCount(fileId, varId);
-							for (size_t i = 0; i < attrCount; i++) {
-								const string attrName = NetCDF::getAttributeName(fileId, varId, i);
-								const Attribute attr = NetCDF::getAttribute(fileId, varId, attrName);
-								context.getLogging().info("Adding attribute '" + attr.toString() + "' to variable '" + varName + "'", getId());
-								variableDescriptor->addAttribute(attr);
-							}
-							// Add variable to segment
-							Segment& segment = context.getSegment(segmentName);
-							context.getLogging().info("Adding accessor for " + variableDescriptor->toString() + " to segment " + segment.toString(), getId());
-							segment.addVariable(*variableDescriptor);
-							ncVarIdMap[varName] = varId;
-						}
-			}
+	        // Copy variable attributes to dictionary
+	        const int type = NetCDF::getVariableType(fileId, varId);
+	        variableDescriptor->setType(type);
+	        const size_t attrCount = NetCDF::getAttributeCount(fileId, varId);
+	        for (size_t i = 0; i < attrCount; i++) {
+	            const string attrName = NetCDF::getAttributeName(fileId, varId, i);
+	            const Attribute attr = NetCDF::getAttribute(fileId, varId, attrName);
+	            context.getLogging().info("Adding attribute '" + attr.toString() + "' to variable '" + varName + "'", getId());
+	            variableDescriptor->addAttribute(attr);
+	        }
+	        // Add variable to segment
+	        Segment& segment = context.getSegment(segmentName);
+	        context.getLogging().info("Adding accessor for " + variableDescriptor->toString() + " to segment " + segment.toString(), getId());
+	        segment.addVariable(*variableDescriptor);
+	        ncVarIdMap[varName] = varId;
+	    }
+	}
 }
 
 void SynL1Reader::stop(Context& context) {
@@ -155,40 +153,38 @@ void SynL1Reader::process(Context& context) {
 	valarray<size_t> starts;
 	valarray<size_t> counts;
 
-	foreach(SegmentDescriptor* segmentDescriptor, segmentDescriptors)
-			{
-				const Segment& segment = context.getSegment(segmentDescriptor->getName());
-				const Grid& grid = segment.getGrid();
-				if (!context.hasLastComputedL(segment, *this) || context.getLastComputedL(segment, *this) < grid.getMinInMemoryL() + grid.getSizeL() - 1) {
-					const vector<VariableDescriptor*> variableDescriptors = segmentDescriptor->getVariableDescriptors();
-					const long firstL = segment.getGrid().getMinInMemoryL();
-					context.getLogging().debug("Segment [" + segment.toString() + "]: firstL = " + lexical_cast<string>(firstL), getId());
-					const long lastL = segment.getGrid().getMaxInMemoryL();
-					context.getLogging().debug("Segment [" + segment.toString() + "]: lastL = " + lexical_cast<string>(lastL), getId());
+	foreach(SegmentDescriptor* segmentDescriptor, segmentDescriptors) {
+	    const Segment& segment = context.getSegment(segmentDescriptor->getName());
+	    const Grid& grid = segment.getGrid();
+	    if (!context.hasLastComputedL(segment, *this) || context.getLastComputedL(segment, *this) < grid.getMinInMemoryL() + grid.getSizeL() - 1) {
+	        const vector<VariableDescriptor*> variableDescriptors = segmentDescriptor->getVariableDescriptors();
+	        const long firstL = segment.getGrid().getMinInMemoryL();
+	        context.getLogging().debug("Segment [" + segment.toString() + "]: firstL = " + lexical_cast<string>(firstL), getId());
+	        const long lastL = segment.getGrid().getMaxInMemoryL();
+	        context.getLogging().debug("Segment [" + segment.toString() + "]: lastL = " + lexical_cast<string>(lastL), getId());
 
-					foreach(VariableDescriptor* variableDescriptor, variableDescriptors)
-							{
-								const string varName = variableDescriptor->getName();
-								const string ncFileName = variableDescriptor->getNcFileBasename();
+	        foreach(VariableDescriptor* variableDescriptor, variableDescriptors) {
+	            const string varName = variableDescriptor->getName();
+	            const string ncFileName = variableDescriptor->getNcFileBasename();
 
-								if (!contains(ncVarIdMap, varName)) {
-									BOOST_THROW_EXCEPTION(logic_error("Unknown variable '" + varName + "'."));
-								}
-								if (!contains(ncFileIdMap, ncFileName)) {
-									BOOST_THROW_EXCEPTION(logic_error("Unknown netCDF file '" + ncFileName + "'."));
-								}
-								const int varId = ncVarIdMap[varName];
-								const int fileId = ncFileIdMap[ncFileName];
-								const size_t dimCount = variableDescriptor->getDimensions().size();
-								Utils::createStartVector(dimCount, firstL, starts);
-								Utils::createCountVector(dimCount, grid.getSizeK(), lastL - firstL + 1, grid.getSizeM(), counts);
-								context.getLogging().progress("Reading variable '" + varName + "' into segment " + segment.toString(), getId());
-								const Accessor& accessor = segment.getAccessor(varName);
-								NetCDF::getVariableData(fileId, varId, starts, counts, accessor.getUntypedData());
-							}
-					context.setLastComputedL(segment, *this, lastL);
-				}
-			}
+	            if (!contains(ncVarIdMap, varName)) {
+	                BOOST_THROW_EXCEPTION(logic_error("Unknown variable '" + varName + "'."));
+	            }
+	            if (!contains(ncFileIdMap, ncFileName)) {
+	                BOOST_THROW_EXCEPTION(logic_error("Unknown netCDF file '" + ncFileName + "'."));
+	            }
+	            const int varId = ncVarIdMap[varName];
+	            const int fileId = ncFileIdMap[ncFileName];
+	            const size_t dimCount = variableDescriptor->getDimensions().size();
+	            Utils::createStartVector(dimCount, firstL, starts);
+	            Utils::createCountVector(dimCount, grid.getSizeK(), lastL - firstL + 1, grid.getSizeM(), counts);
+	            context.getLogging().progress("Reading variable '" + varName + "' into segment " + segment.toString(), getId());
+	            const Accessor& accessor = segment.getAccessor(varName);
+	            NetCDF::getVariableData(fileId, varId, starts, counts, accessor.getUntypedData());
+	        }
+	        context.setLastComputedL(segment, *this, lastL);
+	    }
+	}
 }
 
 int SynL1Reader::getNcFile(const string& ncFileBasename) {
