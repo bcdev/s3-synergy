40a7a700ddca7f622606911ea2d8d176b0db5943
Ralf Quast <ralf.quast@brockmann-consult.de>
2012-02-13 10:10:14 +0100

    - updated VCO job order - fixed VGT S integration test

--- src/test/cpp/integration/VgtS_2_Test.cpp
+++ src/test/cpp/integration/VgtS_2_Test.cpp
@@ -10,7 +10,8 @@
 #include "../../../../src/main/cpp/modules/Pcl.h"
 #include "../../../../src/main/cpp/modules/Vbm.h"
 #include "../../../../src/main/cpp/modules/Vfl.h"
-#include "../../../../src/main/cpp/modules/Vpr.h"
+#include "../../../../src/main/cpp/modules/Vac.h"
+#include "../../../../src/main/cpp/modules/Vco.h"
 #include "../../../../src/main/cpp/reader/SynL1Reader.h"
 #include "../../../../src/main/cpp/util/JobOrderParser.h"
 #include "../../../../src/main/cpp/util/BasicTask.h"
@@ -40,7 +41,8 @@ void VgtS_2_Test::testIntegrationOfVgtSModules() {
 	shared_ptr<Module> pcl = shared_ptr<Module>(new Pcl());
 	shared_ptr<Module> vbm = shared_ptr<Module>(new Vbm());
 	shared_ptr<Module> vfl = shared_ptr<Module>(new Vfl());
-	shared_ptr<Module> vpr = shared_ptr<Module>(new Vpr());
+	shared_ptr<Module> vac = shared_ptr<Module>(new Vac());
+	shared_ptr<Module> vco = shared_ptr<Module>(new Vco());
 	shared_ptr<Module> writer = shared_ptr<Module>(new VgtWriter());
 
 	task.getContext().addModule(reader);
@@ -48,7 +50,8 @@ void VgtS_2_Test::testIntegrationOfVgtSModules() {
 	task.getContext().addModule(pcl);
 	task.getContext().addModule(vbm);
 	task.getContext().addModule(vfl);
-	task.getContext().addModule(vpr);
+	task.getContext().addModule(vac);
+	task.getContext().addModule(vco);
 	task.getContext().addModule(writer);
 
 	const int exitCode = task.execute(Constants::S3_SYNERGY_HOME + "/src/test/resources/jobs/JobOrder.SY_INT_VGTS_2.xml");
