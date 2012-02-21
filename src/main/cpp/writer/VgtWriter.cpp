9708523775b4b71afc3b1102772f96d3745fab98
Ralf Quast <ralf.quast@brockmann-consult.de>
2012-02-21 14:02:00 +0100

    fix!

--- src/main/cpp/writer/VgtWriter.cpp
+++ src/main/cpp/writer/VgtWriter.cpp
@@ -61,10 +61,11 @@ void VgtWriter::defineCoordinateVariables(const Context& context, int fileId, co
 	const VariableDescriptor& crsDescriptor = productDescriptor.getSegmentDescriptor("CRS").getVariableDescriptor("crs");
 	const int crsId = NetCDF::defineVariable(fileId, crsDescriptor.getName(), crsDescriptor.getType());
 
-	//NetCDF::putAttribute(fileId, crsId, crsDescriptor.getAttribute("grid_mapping_name"));
-	NetCDF::putAttribute(fileId, crsId, crsDescriptor.getAttribute("longitude_of_prime_meridian"));
-	//NetCDF::putAttribute(fileId, crsId, crsDescriptor.getAttribute("semi_major_axis"));
-	//NetCDF::putAttribute(fileId, crsId, crsDescriptor.getAttribute("inverse_flattening"));
+	foreach (const Attribute* attribute, crsDescriptor.getAttributes())
+			{
+				context.getLogging().debug("Defining attribute '" + attribute->getName() + "'", getId()); // do not remove this line
+				NetCDF::putAttribute(fileId, crsId, *attribute);
+			}
 }
 
 void VgtWriter::writeCoordinateVariables(Context& context, int fileId, const ProductDescriptor& productDescriptor, const string& segmentName) const {
