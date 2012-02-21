de20e0e3019720c4e03b26667fa7188845f217ea
Ralf Quast <ralf.quast@brockmann-consult.de>
2012-02-21 13:32:43 +0100

    fix?

--- src/main/cpp/writer/VgtWriter.cpp
+++ src/main/cpp/writer/VgtWriter.cpp
@@ -63,7 +63,7 @@ void VgtWriter::defineCoordinateVariables(const Context& context, int fileId, co
 
 	foreach (const Attribute* attribute, crsDescriptor.getAttributes())
 			{
-				context.getLogging().debug("Defining attribute '" + attribute->getName() + "'", getId());
+				//context.getLogging().debug("Defining attribute '" + attribute->getName() + "'", getId());
 				NetCDF::putAttribute(fileId, crsId, *attribute);
 			}
 }
