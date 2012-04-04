ae5b4b7a75a37ef66f59488d73dae3ee158db527
Ralf Quast <ralf.quast@brockmann-consult.de>
2012-04-04 18:56:52 +0200

    removed out-commented code

--- src/main/cpp/modules/Col.cpp
+++ src/main/cpp/modules/Col.cpp
@@ -306,9 +306,6 @@ void Col::addOlciVariables(Context& context) {
 	}
 
 	addVariableAlias(context, targetSegment, "OLC_confidence", sourceSegment, "OLC_confidence");
-
-//	addVariableAlias(context, targetSegment, "longitude", sourceSegment, "longitude");
-//	addVariableAlias(context, targetSegment, "latitude", sourceSegment, "latitude");
 	addVariableAlias(context, targetSegment, "altitude", sourceSegment, "altitude");
 
 	const Segment& olcInfoSegment = context.getSegment(Constants::SEGMENT_OLC_INFO);
