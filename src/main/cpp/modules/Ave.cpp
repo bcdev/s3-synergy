c04f3db76b87f0e3d36a3cc3d213e8225af82ee1
Ralf Quast <ralf.quast@brockmann-consult.de>
2012-05-09 18:29:47 +0200

    update

--- src/main/cpp/modules/Ave.cpp
+++ src/main/cpp/modules/Ave.cpp
@@ -40,7 +40,7 @@ void Ave::start(Context& context) {
 	const size_t sizeM = ceil(sourceGrid.getSizeM() / double(averagingFactor));
 	const size_t sizeK = sourceGrid.getSizeK();
 	const size_t maxL = ceil((sourceGrid.getMaxL() - sourceGrid.getMinL() + 1) / double(averagingFactor)) - 1;
-	targetSegment = &context.addMapSegment(Constants::SEGMENT_SYN_AVERAGED, maxL + 1, sizeM, sizeK);
+	targetSegment = &context.addSwathSegment(Constants::SEGMENT_SYN_AVERAGED, maxL + 1, sizeM, sizeK, 0, maxL);
 
 	addVariables(context);
 }
